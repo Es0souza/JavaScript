26e38d848e5b6776f2d7116922c8453143aef71d
Edgard Souza <edgard.souzalopes@gmail.com>
2023-04-04 20:52:40 -0300

    FEAT: Dropdown Sub-menu and responsive outside of click function

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initTabNav from "./modules/tabnav.js";
 import initAccordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -11,3 +12,4 @@ initTabNav();
 initAccordion();
 initModal();
 initToolTip();
+initDropdownMenu();
